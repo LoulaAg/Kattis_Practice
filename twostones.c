746a28d85c8b03e6d08db7078341cdefe70cb303
LoulaAg <loula.ag.13@gmail.com>
2017-07-17 17:03:04 -0600

    Add files via upload

---
+++ twostones.c
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+
+void main(){
+  //char stones;
+  int num;
+  if( 1== scanf("%d", &num)){
+    //num= (int)stones;
+    if ( num % 2 == 1){
+      printf("Alice\n");
+    }
+    else{
+      printf("Bob\n");
+    }
+  }
+}
